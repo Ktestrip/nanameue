a39dc132564b42cddcad5da650e9e2dee257b621
Frqnck <franck.petriz@ekonoo.com>
2022-08-30 21:07:17 +0200

    [feat/login-view] create assets for color and logo

---
+++ Nanameue/NanameueTests/Login/LoginViewControllerTests.swift
@@ -0,0 +1,48 @@
+//
+//  LoginViewControllerTests.swift
+//  NanameueTests
+//
+//  Created by Frqnck  on 30/08/2022.
+//
+
+import Foundation
+@testable import Nanameue
+import XCTest
+
+class LoginViewControllerTests: XCTestCase {
+    
+    private func setupView() -> LoginViewController {
+        let vc = LoginViewController(nibName: "LoginViewController", bundle: nil)
+        //to trigger view will appear on vc, just add it to a view
+        let view = UIView()
+        view.addSubview(vc.view)
+        return vc
+    }
+    
+    func testPlaceholder() {
+        let vc = self.setupView()
+        XCTAssertEqual(vc.passwordTextField.placeholder, "login_password_placeholder".translate)
+        XCTAssertEqual(vc.emailTextField.placeholder , "login_email_placeholder".translate)
+        XCTAssertEqual(vc.connectButton.title(for: .normal), "login_connect_button".translate)
+        XCTAssertEqual(vc.createAccountButton.title(for: .normal), "login_create_account_button".translate)
+    }
+    
+    func testBackgroundColor() {
+        let vc = self.setupView()
+        XCTAssertEqual(vc.view.backgroundColor , UIColor(named: "mainColor"))
+    }
+    
+    func testPasswordIsSecureEntry() {
+        let vc = self.setupView()
+        XCTAssertTrue(vc.passwordTextField.isSecureTextEntry)
+    }
+    
+    func testEmailTextFieldSetup() {
+        let vc = self.setupView()
+        XCTAssertEqual(vc.emailTextField.autocorrectionType, .no)
+        XCTAssertEqual(vc.emailTextField.spellCheckingType, .no)
+        XCTAssertEqual(vc.emailTextField.keyboardType, .emailAddress)
+        XCTAssertEqual(vc.emailTextField.textContentType, .emailAddress)
+        
+    }
+}
