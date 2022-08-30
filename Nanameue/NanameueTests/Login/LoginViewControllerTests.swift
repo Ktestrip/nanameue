40ea2acbdf0400975ef86674f5f40058ad87bcad
Frqnck <franck.petriz@ekonoo.com>
2022-08-30 22:33:31 +0200

    [feat/login-logic] update unit test to ensure check before calling login method

--- Nanameue/NanameueTests/Login/LoginViewControllerTests.swift
+++ Nanameue/NanameueTests/Login/LoginViewControllerTests.swift
@@ -43,6 +43,35 @@ class LoginViewControllerTests: XCTestCase {
         XCTAssertEqual(vc.emailTextField.spellCheckingType, .no)
         XCTAssertEqual(vc.emailTextField.keyboardType, .emailAddress)
         XCTAssertEqual(vc.emailTextField.textContentType, .emailAddress)
-        
     }
+    
+    func testEmptyEmailLoginShouldNotCallLoginProvider() {
+        let vc = self.setupView()
+        let mock = LoginProviderMock()
+        vc.loginProvider = mock
+        vc.passwordTextField.text = "test"
+        vc.connectButton.sendActions(for: .touchUpInside)
+        XCTAssertFalse(mock.performLoginHasBeenCalled)
+    }
+    
+    func testEmptyPasswordShouldNotCallLoginProvider() {
+        let vc = self.setupView()
+        let mock = LoginProviderMock()
+        vc.loginProvider = mock
+        vc.emailTextField.text = "test"
+        vc.connectButton.sendActions(for: .touchUpInside)
+        XCTAssertFalse(mock.performLoginHasBeenCalled)
+    }
+    
+    func testShouldCallLoginProvider() {
+        let vc = self.setupView()
+        let mock = LoginProviderMock()
+        vc.loginProvider = mock
+        vc.emailTextField.text = "test"
+        vc.passwordTextField.text = "test"
+        // both textfield are setup, should satisfy condition for loginprovider call
+        vc.connectButton.sendActions(for: .touchUpInside)
+        XCTAssertTrue(mock.performLoginHasBeenCalled)
+    }
+
 }
