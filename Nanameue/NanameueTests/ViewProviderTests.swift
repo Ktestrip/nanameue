b8ad0a37039308315ae6a4f262719275f148eb51
Frqnck <franck.petriz@ekonoo.com>
2022-08-30 23:25:59 +0200

    [feat/account-creation-view-setup] added some test to viewProvider to check CreateAccountViewControlelr

--- Nanameue/NanameueTests/ViewProviderTests.swift
+++ Nanameue/NanameueTests/ViewProviderTests.swift
@@ -21,4 +21,15 @@ class ViewProviderTests: XCTestCase {
         // check that login provider has been ... provided
         XCTAssertNotNil(castedView.loginProvider)
     }
+    
+    func testCreateAccountViewControllerConfiguration() {
+        let vc = ViewProvider.getViewController(view: .createAccountViewController)
+        guard let castedView = vc as? CreateAccountViewController else {
+            // not the good type of object
+            XCTAssert(true, "viewController is not a CreateAccountViewController")
+            return
+        }
+        // check that login provider has been ... provided
+        XCTAssertNotNil(castedView.loginProvider)
+    }
 }
