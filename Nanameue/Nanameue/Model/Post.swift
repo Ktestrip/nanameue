d451185065b8f665284379967958c8b5015cf9e4
Frqnck <franck.petriz@ekonoo.com>
2022-09-01 21:34:09 +0200

    [feat/post-provider] Model for futur Post from firebase

---
+++ Nanameue/Nanameue/Model/Post.swift
@@ -0,0 +1,17 @@
+//
+//  Post.swift
+//  Nanameue
+//
+//  Created by Frqnck  on 01/09/2022.
+//
+
+import Foundation
+import UIKit
+
+struct Post {
+    var id: String = ""
+    var content: String = ""
+    var imageUrl: String?
+    var date = Date()
+    var image: UIImage?
+}
