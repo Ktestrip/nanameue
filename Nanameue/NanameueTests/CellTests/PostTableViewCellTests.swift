167dff1d100121c8e71aa1b14730db1482ca9e8f
Frqnck <franck.petriz@ekonoo.com>
2022-09-04 12:11:00 +0200

    [feat/post-cell-design] added test for postcell + added helper to easily init cell for unit test purpose only

---
+++ Nanameue/NanameueTests/CellTests/PostTableViewCellTests.swift
@@ -0,0 +1,42 @@
+//
+//  PostTableViewCellTests.swift
+//  NanameueTests
+//
+//  Created by Frqnck  on 04/09/2022.
+//
+
+import UIKit
+import XCTest
+@testable import Nanameue
+
+class PostTableViewCellTests: XCTestCase {
+    
+    func testSetupViewNoImage() {
+        let tableView = TableViewControllerTest(nib: PostTableViewCell.nib, identifier: PostTableViewCell.identifier)
+        let cell = tableView.createCell(indexPath: IndexPath(row: 0, section: 0)) as! PostTableViewCell
+        let post = Post(content: "test")
+        cell.setupCell(post: post)
+        XCTAssertEqual(cell.contentLabel.text, "test")
+        XCTAssertNotEqual(cell.dateLabel.text, "")
+        XCTAssertEqual(cell.imageViewHeightConstraint.constant, 0)
+    }
+    
+    func testSetupViewWithImage() {
+        let tableView = TableViewControllerTest(nib: PostTableViewCell.nib, identifier: PostTableViewCell.identifier)
+        let cell = tableView.createCell(indexPath: IndexPath(row: 0, section: 0)) as! PostTableViewCell
+        let post = Post(content: "test")
+        post.setImageUrl(url: URL(fileURLWithPath: ""))
+        cell.setupCell(post: post)
+        XCTAssertEqual(cell.contentLabel.text, "test")
+        XCTAssertNotEqual(cell.dateLabel.text, "")
+        XCTAssertNotEqual(cell.imageViewHeightConstraint.constant, 0)
+        XCTAssertEqual(cell.imageViewHeightConstraint.constant, 128)
+    }
+    
+    func testDeleteButtonImage() {
+        let tableView = TableViewControllerTest(nib: PostTableViewCell.nib, identifier: PostTableViewCell.identifier)
+        let cell = tableView.createCell(indexPath: IndexPath(row: 0, section: 0)) as! PostTableViewCell
+        XCTAssertNotNil(cell.deleteButton.imageView)
+        XCTAssertEqual(cell.deleteButton.title(for: .normal), "")
+    }
+}
