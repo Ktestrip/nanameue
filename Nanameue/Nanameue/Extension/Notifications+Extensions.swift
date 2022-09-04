9cc940ad5fa7430bd51e27f8063ba6949bc16768
Frqnck <franck.petriz@ekonoo.com>
2022-09-04 13:54:51 +0200

    [feat/error-view-design] create extension for notifications name for better usage

---
+++ Nanameue/Nanameue/Extension/Notifications+Extensions.swift
@@ -0,0 +1,12 @@
+//
+//  Notifications+Extensions.swift
+//  Nanameue
+//
+//  Created by Frqnck  on 04/09/2022.
+//
+
+import Foundation
+
+extension Notification.Name {
+    static let errorInformation = Notification.Name("ErrorInformation")
+}
